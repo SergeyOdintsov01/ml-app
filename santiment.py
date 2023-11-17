b56cd13192921e9966bc97ba4d17815073861a5f
Sergey Odintsov <ivanivanovplayer2019@gmail.com>
2023-11-17 17:10:12 +0100

    Добавил функцию вывода

--- santiment.py
+++ santiment.py
@@ -1,5 +1,6 @@
 from transformers import pipeline
 
 classifier = pipeline("sentiment-analysis", "blanchefort/rubert-base-cased-sentiment")
-classifier("Я изучаю модели манного обучения и программную инженерию")
+output = classifier("Я изучаю модели манного обучения и программную инженерию")
+print(output)
 
