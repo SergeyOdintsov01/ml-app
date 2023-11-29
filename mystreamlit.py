0da0d5fc58593d0e74f061e558e12229c0c34692
Sergey Odintsov <ivanivanovplayer2019@gmail.com>
2023-11-29 17:36:49 +0100

    Add st.text_input and st.write()

--- mystreamlit.py
+++ mystreamlit.py
@@ -4,20 +4,11 @@ from transformers import pipeline
 
 
 st.title('RU/ENG translation')
-
-def input_text():
-    '''Создание формы для взфимодействия с переводчиком translate.py'''
-    pass
+text_from_st = st.text_input('Text from streamlit') #текст который ввел пользователь на страничке в браузере
     
 translator = pipeline("translation_ru_to_en", "Helsinki-NLP/opus-mt-ru-en")
 
-def input_text():
-    t = input('Введите текст на русском: ')
-    return t
-
 def translate_model(ruText):
-    print('Перевод текста на английский:{0} '.format(translator(ruText)[0]['translation_text']))
+    st.write('Переведенный текст :',translator(ruText)[0]['translation_text'])
 
-
-text = input_text()
-translate_model(text)
+translate_model(text_from_st)
