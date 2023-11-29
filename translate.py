6fddc1eb53f2a4c7b5fa266773e2eb3ec8a70bd0
Sergey Odintsov <ivanivanovplayer2019@gmail.com>
2023-11-29 16:42:19 +0100

    Add 2 functions. Modified file

--- translate.py
+++ translate.py
@@ -2,4 +2,14 @@ from transformers import pipeline
 
 
 translator = pipeline("translation_ru_to_en", "Helsinki-NLP/opus-mt-ru-en")
-print(translator("Этот текст должна превести на английский язык модель машинного обучения"))
+
+def input_text():
+    t = input('Введите текст на русском: ')
+    return t
+
+def translate_model(ruText):
+    print('Перевод текста на английский:{0} '.format(translator(ruText)[0]['translation_text']))
+
+
+text = input_text()
+translate_model(text)
