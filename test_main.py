d1aa561844ef8203d8995982d722acf2122a2b27
Sergey Odintsov <ivanivanovplayer2019@gmail.com>
2023-12-20 00:38:11 +0100

    Change func

--- test_main.py
+++ test_main.py
@@ -9,12 +9,13 @@ def test_read_main():
     assert response.status_code == 200
     assert response.json() == {"message": "This model translation text"}
 
+#Correct output
 def test_translate_text():
     response = client.post("/translate/",
         json={"text": "Машина"}
     )
     json_data = response.json() 
-
+    
     assert response.status_code == 200
     assert json_data["translation_text"] == "Car"
 
