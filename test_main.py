8740453c1013932b97e36f7843c99d8e0d78c15a
Ivan <ivanivanovplayer2019@gmail.com>
2024-03-24 21:13:29 +0100

    check test_main flake8

--- test_main.py
+++ test_main.py
@@ -3,19 +3,19 @@ from main import app
 
 client = TestClient(app)
 
-#Actions
+
+# Actions
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "This model translation text"}
 
-#Correct output
+
+# Correct output
 def test_translate_text():
     response = client.post("/translate/",
-        json={"text": "Машина"}
-    )
-    json_data = response.json() 
-    
+                           json={"text": "Машина"})
+    json_data = response.json()
+
     assert response.status_code == 200
     assert json_data["translation_text"] == "Car"
-
