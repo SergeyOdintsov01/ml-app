fa6491071447906080d53bba0ec67e547072dbb9
Ivan <ivanivanovplayer2019@gmail.com>
2024-03-24 20:53:42 +0100

    check flake8 santiment.py

--- santiment.py
+++ santiment.py
@@ -1,6 +1,8 @@
 from transformers import pipeline
 
-classifier = pipeline("sentiment-analysis", "blanchefort/rubert-base-cased-sentiment")
+
+classifier = pipeline("sentiment-analysis",
+                      "blanchefort/rubert-base-cased-sentiment")
+
 output = classifier("Я изучаю модели манного обучения и программную инженерию")
 print(output)
-
