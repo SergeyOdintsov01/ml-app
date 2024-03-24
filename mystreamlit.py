a7d7d815f4f1bbdb9cfc3a09439adf23f9457aae
Ivan <ivanivanovplayer2019@gmail.com>
2024-03-24 21:38:05 +0100

    check mystreamlit.py by flake8

--- mystreamlit.py
+++ mystreamlit.py
@@ -1,14 +1,16 @@
-import io
 import streamlit as st
 from transformers import pipeline
 
 
 st.title('RU/ENG translation')
-text_from_st = st.text_input('Text from streamlit') #текст который ввел пользователь на страничке в браузере
-    
+# текст который вводит пользователь на страничке в браузере
+text_from_st = st.text_input('Text from streamlit')
+
 translator = pipeline("translation_ru_to_en", "Helsinki-NLP/opus-mt-ru-en")
 
+
 def translate_model(ruText):
-    st.write('Переведенный текст :',translator(ruText)[0]['translation_text'])
+    st.write('Переведенный текст :', translator(ruText)[0]['translation_text'])
+
 
 translate_model(text_from_st)
